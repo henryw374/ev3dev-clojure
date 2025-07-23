40c05f10431ad93d3ff5e3a6f0111f93ac617446
Henry Widd <henryw374@gmail.com>
2025-07-23 18:04:17 +0100

    start playing

---
+++ README.md
@@ -0,0 +1,17 @@
+# ev3 clojure 
+
+* setup [ev3dev](https://www.ev3dev.org/docs/getting-started/)
+* makefile cmd to create uber and copy it to device
+* makefile cmd to connect
+
+```shell
+robot@ev3dev:~$ java -jar lib1-standalone.jar 
+```
+
+* this may take minutes
+    * this may take minutes to connect. you will see 'starting' printed eventually
+* connect socket repl 192.168.2.2:7091
+  * may take minutes before you see prompt
+* see play ns. load it into repl. may take time...
+* run cmds!
+ 
--- src/com/widdindustries/play.clj
+++ src/com/widdindustries/play.clj
@@ -2,14 +2,16 @@
   (:import (ev3dev.actuators.lego.motors EV3LargeRegulatedMotor)
            (lejos.hardware.port MotorPort)))
 
-(defonce motor-a (EV3LargeRegulatedMotor. MotorPort/A))
+(def motor-a (EV3LargeRegulatedMotor. MotorPort/A))
 
 
 (comment 
-  (EV3LargeRegulatedMotor/.setSpeed motor-a 200)
+  (EV3LargeRegulatedMotor/.setSpeed motor-a 800)
   (EV3LargeRegulatedMotor/.forward motor-a)
   
+  ; what's the difference?
   (EV3LargeRegulatedMotor/.stop motor-a)
-  ;(EV3LargeRegulatedMotor/. motor-a)
+  (EV3LargeRegulatedMotor/.brake motor-a)
+  
   
   )
